5cd4f6730733ae6d0ad1199c6478f0b0dd148ac4
Xiu Hong <xiuhong.kooi977055@gmail.com>
2020-05-25 14:28:33 +0800

    Add retrive info method

--- pkg/app/core.go
+++ pkg/app/core.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"errors"
 	"fmt"
 	c "github.com/kooixh/genid/pkg/constants"
 	g "github.com/kooixh/genid/pkg/generators"
@@ -8,11 +9,20 @@ import (
 	"github.com/kooixh/genid/utils/redis"
 )
 
+const coreAppVersion = "0.1"
+
 type CalibrationSettings struct {
 	Initial int
 	Offset int
 	Total int
 	IdType string
+}
+
+type CoreInfo struct {
+	Settings *CalibrationSettings
+	Status string
+	Version string
+	NextId string
 }
 
 func (cal *CalibrationSettings) Validate() (bool, string) {
@@ -27,6 +37,13 @@ func (cal *CalibrationSettings) Validate() (bool, string) {
 
 func GenerateNewId(idChannel chan string, refillChannel chan int) {
 	ids := redis.LPop(c.IdListKey)
+	if ids.Err() != nil {
+		idChannel <- ""
+		refillChannel <- 0
+		return
+	}
+	idChannel <- ids.Val()
+
 	refillThreshold, err := redis.Get(c.IdListKey).Int()
 	if err != nil {
 		refillThreshold = 10
@@ -36,7 +53,6 @@ func GenerateNewId(idChannel chan string, refillChannel chan int) {
 	} else {
 		refillChannel <- 0
 	}
-	idChannel <- ids.Val()
 }
 
 func Calibrate(cal CalibrationSettings, refillChannel chan int) {
@@ -44,37 +60,81 @@ func Calibrate(cal CalibrationSettings, refillChannel chan int) {
 	redis.Set(c.InitialKey, cal.Initial)
 	redis.Set(c.TotalKey, cal.Total)
 	redis.Set(c.GeneratorTypeKey, cal.IdType)
+	redis.Set(c.CalibratedKey, true)
 	go Refill(refillChannel)
 
 }
 
-func Refill(finished chan int) {
+func Refill(refillChannel chan int) {
 
-	offset, errOffset := redis.Get(c.OffsetKey).Int()
-	total, errTotal := redis.Get(c.TotalKey).Int()
-	initial, errInitial := redis.Get(c.InitialKey).Int()
-	if errOffset != nil || errTotal != nil || errInitial != nil {
-		finished <- c.RedisErrorCode
+	settings, err := retrieveCalibrationSettings()
+	if err != nil {
+		refillChannel <- c.RedisErrorCode
+		return
 	}
+	rawIds := utils.GenerateNewIdSet(settings.Total, settings.Offset, settings.Initial)
 
-	rawIds := utils.GenerateNewIdSet(total, offset, initial)
-
-	generatorType := redis.Get(c.GeneratorTypeKey).Val()
 	var generator g.Generator
-	if generatorType == c.GeneratorTypeAlphaNum {
+	if settings.IdType == c.GeneratorTypeAlphaNum {
 		generator = new(g.AlphaNumericIdGenerator)
-	} else if generatorType == c.GeneratorTypeNum {
+	} else if settings.IdType == c.GeneratorTypeNum {
 		generator = new(g.NumericIdGenerator)
 	} else {
 		fmt.Println("error, generator type is unknown")
-		finished <- c.RedisErrorUnknownTypeCode
+		refillChannel <- c.RedisErrorUnknownTypeCode
+		return
 	}
 
 	generatedIds := generator.Generate(rawIds)
 	for _, elem := range generatedIds {
 		redis.RPush(c.IdListKey, elem)
 	}
-	fmt.Println("generation done, increasing offset")
-	redis.Set(c.OffsetKey, offset + 1)
-	finished <- 0
+	redis.Set(c.OffsetKey, settings.Offset + 1)
+	refillChannel <- 0
+
+}
+
+func ReturnAppInfo() (*CoreInfo, error) {
+	var info *CoreInfo
+	info = new(CoreInfo)
+	status := redis.Get(c.CalibratedKey)
+	var statValue string
+	if status.Err() != nil {
+		statValue = c.NotCalibrated
+	} else {
+		statValue = c.Calibrated
+	}
+	info.Status = statValue
+	info.Version = coreAppVersion
+	if statValue == c.NotCalibrated {
+		return info, nil
+	}
+	settings, err := retrieveCalibrationSettings()
+	if err != nil {
+		return nil, errors.New("unable to retrieve calibration settings from Redis")
+	}
+	info.Settings = settings
+	nextId := redis.Peek(c.IdListKey)
+	if nextId.Err() != nil {
+		info.NextId = ""
+	} else {
+		info.NextId = nextId.Val()[0]
+	}
+	return info, nil
+}
+
+func retrieveCalibrationSettings() (*CalibrationSettings, error){
+	offset, errOffset := redis.Get(c.OffsetKey).Int()
+	total, errTotal := redis.Get(c.TotalKey).Int()
+	initial, errInitial := redis.Get(c.InitialKey).Int()
+	generatorType := redis.Get(c.GeneratorTypeKey).Val()
+	if errOffset != nil || errTotal != nil || errInitial != nil {
+		return nil, errors.New("unable to retrieve calibration settings from Redis")
+	}
+	return &CalibrationSettings{
+		Initial: initial,
+		Offset: offset,
+		Total: total,
+		IdType: generatorType,
+	}, nil
 }
