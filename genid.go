566d18229c902689d8964ba34249ba60eaab6af6
Xiu Hong <xiuhong.kooi977055@gmail.com>
2020-05-25 14:29:39 +0800

    Add info flag to program entrypoint

--- genid.go
+++ genid.go
@@ -5,6 +5,7 @@ import (
 	ap "github.com/akamensky/argparse"
 	core "github.com/kooixh/genid/pkg/app"
 	c "github.com/kooixh/genid/pkg/constants"
+	"github.com/kooixh/genid/utils/redis"
 	"os"
 )
 
@@ -14,6 +15,7 @@ func main() {
 	parser := ap.NewParser("main", "Configuration provided for core genid")
 	calibrate := parser.Flag("c", "calibrate", &ap.Options{Required: false, Help: "Initiate calibration"})
 	refill := parser.Flag("r", "refill", &ap.Options{Required: false, Help: "Refill ids"})
+	info := parser.Flag("i", "info", &ap.Options{Required: false, Help: "Show info of app"})
 	initial := parser.Int("", "initial", &ap.Options{Required: false, Help: "Initial starting number for id",
 		Default: 100})
 	total  := parser.Int("", "total", &ap.Options{Required: false, Help: "Total number stored each refill",
@@ -23,6 +25,12 @@ func main() {
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
+		os.Exit(1)
+	}
+
+	res := redis.Ping()
+	if res.Err() != nil {
+		fmt.Println("error connecting to Redis database")
 		os.Exit(1)
 	}
 
@@ -43,9 +51,28 @@ func main() {
 		exit := <-refillChannel
 		os.Exit(exit)
 	} else if *refill {
+		fmt.Println("starting refill")
 		go core.Refill(refillChannel)
 		exit := <-refillChannel
+		fmt.Println("refill done")
 		os.Exit(exit)
+	} else if *info {
+		appInfo, err := core.ReturnAppInfo()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println("Status:", appInfo.Status)
+		fmt.Println("Version:", appInfo.Version)
+		if appInfo.Status == c.Calibrated {
+			fmt.Println("Next Id:", appInfo.NextId)
+			settings := appInfo.Settings
+			fmt.Println("Initial Id:", settings.Initial)
+			fmt.Println("Id Type:", settings.IdType)
+			fmt.Println("Offset:", settings.Offset)
+			fmt.Println("Refill Amount:", settings.Total)
+		}
+		os.Exit(0)
 	}
 
 	idChannel := make(chan string)
@@ -53,4 +80,5 @@ func main() {
 	fmt.Println("id generated is " + <-idChannel)
 	exit := <-refillChannel
 	os.Exit(exit)
+	
 }
