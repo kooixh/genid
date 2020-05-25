e0f86bcb542e448de65bf44f8c0146e3443620eb
Xiu Hong <xiuhong.kooi977055@gmail.com>
2020-05-25 15:22:22 +0800

    Refactor main method to use helper function for flags

--- genid.go
+++ genid.go
@@ -36,50 +36,13 @@ func main() {
 
 	refillChannel := make(chan int)
 	if *calibrate {
-		fmt.Println("Starting calibration")
-		settings := core.CalibrationSettings{
-			Initial: *initial,
-			Offset: c.InitialOffset,
-			Total: *total,
-			IdType: *idType,
-		}
-		validate, msg := settings.Validate()
-		if !validate {
-			fmt.Println(msg)
-			os.Exit(1)
-		}
-		core.Calibrate(settings, refillChannel)
-		exit := <-refillChannel
-		fmt.Println("Calibration completed")
-		os.Exit(exit)
+		os.Exit(initCalibration(*initial, *total, *idType, refillChannel))
 	} else if *refill {
-		fmt.Println("Starting refill")
-		if !core.CheckIsCalibrated() {
-			fmt.Println("Genid is not calibrated, calibrate before using")
-			os.Exit(1)
-		}
-		go core.Refill(refillChannel)
-		exit := <-refillChannel
-		fmt.Println("Refill done")
-		os.Exit(exit)
+		os.Exit(initRefill(refillChannel))
 	} else if *info {
-		appInfo, err := core.ReturnAppInfo()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		fmt.Println("Status:", appInfo.Status)
-		fmt.Println("Version:", appInfo.Version)
-		if appInfo.Status == c.Calibrated {
-			fmt.Println("Next Id:", appInfo.NextId)
-			settings := appInfo.Settings
-			fmt.Println("Initial Id:", settings.Initial)
-			fmt.Println("Id Type:", settings.IdType)
-			fmt.Println("Offset:", settings.Offset)
-			fmt.Println("Refill Amount:", settings.Total)
-		}
-		os.Exit(0)
+		os.Exit(initPrintInfo())
 	}
+
 	if !core.CheckIsCalibrated() {
 		fmt.Println("Genid is not calibrated, calibrate before using")
 		os.Exit(1)
@@ -89,5 +52,55 @@ func main() {
 	fmt.Println("id generated is " + <-idChannel)
 	exit := <-refillChannel
 	os.Exit(exit)
-	
+}
+
+
+func initCalibration(initial int, total int, idType string, refillChannel chan int) int {
+	settings := core.CalibrationSettings{
+		Initial: initial,
+		Offset: c.InitialOffset,
+		Total: total,
+		IdType: idType,
+	}
+	fmt.Println("Starting Genid calibration with settings", settings)
+	validate, msg := settings.Validate()
+	if !validate {
+		fmt.Println(msg)
+		return 1
+	}
+	core.Calibrate(settings, refillChannel)
+	exit := <-refillChannel
+	fmt.Println("Completed Genid Calibration")
+	return exit
+}
+
+func initRefill(refillChannel chan int) int {
+	fmt.Println("Refilling the lists of Ids")
+	if !core.CheckIsCalibrated() {
+		fmt.Println("Genid is not calibrated, calibrate before using")
+		return 1
+	}
+	go core.Refill(refillChannel)
+	exit := <-refillChannel
+	fmt.Println("Successfully refilled the lists of Ids")
+	return exit
+}
+
+func initPrintInfo() int {
+	appInfo, err := core.ReturnAppInfo()
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	fmt.Println("Status:", appInfo.Status)
+	fmt.Println("Version:", appInfo.Version)
+	if appInfo.Status == c.Calibrated {
+		fmt.Println("Next Id:", appInfo.NextId)
+		settings := appInfo.Settings
+		fmt.Println("Initial Id:", settings.Initial)
+		fmt.Println("Id Type:", settings.IdType)
+		fmt.Println("Offset:", settings.Offset)
+		fmt.Println("Refill Amount:", settings.Total)
+	}
+	return 0
 }
